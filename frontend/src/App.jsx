da4e383b52b24062fad9fc7c3603d637d8dbff2a
guru-9392 <balagurivireddy.9392@gmail.com>
2026-06-21 06:00:18 +0530

    all ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import Navbar from './components/shared/Navbar'
-import Login from './components/auth/login'
+import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
